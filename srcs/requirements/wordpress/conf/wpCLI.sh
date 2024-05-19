9e928ae2a019afc0df3e95f8e27c6d538a5b855a
kscarmy <guderram@student.42.fr>
2024-05-19 18:13:27 +0200

    Push 07 : wordpress toujours down

--- srcs/requirements/wordpress/conf/wpCLI.sh
+++ srcs/requirements/wordpress/conf/wpCLI.sh
@@ -1,20 +1,48 @@
 #!bin/sh
-sleep 5;
+sleep 10;
+# if [ ! -e /var/www/wordpress/wp-config.php ]; then
+#     cd /var/www/wordpress
+#     wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+#     # sleep 5;
+#     wp core install     --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
+#     wp user create      --allow-root --role=author $MYUSER $MYUSER_MAIL --user_pass=$MYUSER_PASS --path='/var/www/wordpress' >> /log.txt
+#     #Allow users to upload images
+#     chown -R www-data /var/www/wordpress/wp-content/uploads
+# fi
+
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
     cd /var/www/wordpress
+    
+    # Créer le fichier de configuration wp-config.php
     wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
 
-    # sleep 5;
-    wp core install     --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
-    wp user create      --allow-root --role=author $MYUSER $MYUSER_MAIL --user_pass=$MYUSER_PASS --path='/var/www/wordpress' >> /log.txt
-    #Allow users to upload images
-    chown -R www-data /var/www/wordpress/wp-content/uploads
+    # Installer WordPress
+    wp core install --url=$DOMAIN_NAME --title="$SITE_TITLE" --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
+    
+    # Créer un utilisateur avec le rôle d'auteur
+    wp user create --allow-root --role=author $MYUSER $MYUSER_MAIL --user_pass=$MYUSER_PASS --path='/var/www/wordpress' >> /log.txt
+    
+    # Permettre aux utilisateurs de télécharger des images
+    chown -R www-data:www-data /var/www/wordpress/wp-content/uploads
 fi
 
-if [ ! -d /run/php ]; then
-    mkdir /run/php
+if [ -d /run/php ]; then
+    # mkdir /run/php
+	rm -r /run/php
 fi
 
-/usr/sbin/php-fpm7.3 -F
+# service --status-all
+# rm -r /run/php
+# ls -la /run/php/
 
-service php7.2-fpm restart
+# /usr/sbin/php-fpm7.3 -F
+# /usr/sbin/php7.4-fpm -F
+
+# service php7.2-fpm restart
+service php7.4-fpm start
+# /etc/init.d/php7.4-fpm -F
+
+# service --status-all
+
+# /usr/sbin/apache2 -D FOREGROUND
